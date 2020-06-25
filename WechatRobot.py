f748bc7fe0436b601b4b79788324fc01f46f3210
Jerry1014 <757320383@qq.com>
2020-06-25 22:10:28 +0800

    修复 将用户码从代码中分离出来

--- WechatRobot.py
+++ WechatRobot.py
@@ -1,9 +1,10 @@
 from wxpy import *
 
+user_code = '88387'
 bot = Bot(console_qr=1)
 
 kindle = bot.mps().search('Kindle杂志公社')[0]
 print('sending msg')
-kindle.send('88387')
+kindle.send(user_code)
 print('all done')
 bot.logout()
