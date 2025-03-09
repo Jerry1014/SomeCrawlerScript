d95b209183e03fba43afe6a3c5dcb75db7a95de6
Jerry <xiangming.huang@qq.com>
2025-03-10 00:18:31 +0800

    token过期

--- hifi_checkin.py
+++ hifi_checkin.py
@@ -1,5 +1,6 @@
 import json
 import os
+from time import time
 
 from playwright.sync_api import sync_playwright
 
@@ -10,7 +11,12 @@ with sync_playwright() as p:
     # headless=False, slow_mo=50
     browser = p.chromium.launch()
     browser_context = browser.new_context()
+
     # token过期时间 2025-06-17T13:40:56.053Z
+    token_expire_time = 1750138856
+    if time() > token_expire_time:
+        raise Exception("token已过期")
+
     browser_context.add_cookies([
         {"name": "bbs_sid", "value": sid, "domain": "www.hifini.com", "path": "/"}
         , {"name": "bbs_token", "value": token, "domain": "www.hifini.com", "path": "/"}])
