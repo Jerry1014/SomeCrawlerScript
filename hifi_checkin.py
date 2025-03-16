2a733021311539c404aeaf500265e7b1d53f8920
Jerry <xiangming.huang@qq.com>
2025-03-16 22:52:52 +0800

    hifi签到 增加结果返回

--- hifi_checkin.py
+++ hifi_checkin.py
@@ -1,8 +1,7 @@
-import json
 import os
 from time import time
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 sid = os.environ["HIFI_SID"]
 token = os.environ["HIFI_TOKEN"]
@@ -12,20 +11,26 @@ with sync_playwright() as p:
     browser = p.chromium.launch()
     browser_context = browser.new_context()
 
-    # token过期时间 2025-06-17T13:40:56.053Z
+    # token
+    # 过期时间 2025-06-17T13:40:56.053Z
     token_expire_time = 1750138856
     if time() > token_expire_time:
         raise Exception("token已过期")
-
     browser_context.add_cookies([
         {"name": "bbs_sid", "value": sid, "domain": "www.hifini.com", "path": "/"}
         , {"name": "bbs_token", "value": token, "domain": "www.hifini.com", "path": "/"}])
 
-    # 登录页
+    # 签到
     page = browser_context.new_page()
     page.goto("https://www.hifini.com/")
-    page.locator('[id="sign"]').click()
-    page.wait_for_url("**/sg_sign.htm")
-    print(page.title())
+    sign_button_locator = page.locator('[id="sign"]')
+    expect(sign_button_locator).to_have_count(1)
+    sign_button_locator.click()
+
+    # 签到结果
+    tips_locator = page.locator('[class="modal-body"]')
+    tips_locator.wait_for()
+    expect(tips_locator).to_have_count(1)
+    print(tips_locator.text_content().strip())
 
     browser.close()
